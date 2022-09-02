d2f86880c8024dd50446838cb00a4d7055a4cd80
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-09-02 10:10:48 +0530

    Initial commit

---
+++ src/StudentRegistrationMain.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StudentRegistrationMain {
+    public static void main(String[] args) {
+        List<Integer> registrationList= Arrays.asList(595959,552468,5596959,596945,552467);
+        System.out.println("registrationList = " + registrationList);
+        Collections.sort(registrationList);
+        System.out.println("registrationList = " + registrationList);
+        
+
+    }
+}
